7dde64272a216dfcebd90c69ae3c9b66e5d898d9
a1093374 <119493454+a1093374@users.noreply.github.com>
2024-03-13 21:35:00 +0800

    Add files via upload

---
+++ week2.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Week2作業-履歷</title>
+    <style>
+        h1{
+            text-align: center;
+        }
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+        th, td {
+            padding: 8px;
+            text-align: left;
+            border-bottom: 1px solid #ddd;
+            width: 8px;
+        }
+        img {
+            max-width: 500px;
+            max-height: 500px;
+            display: block;
+            margin: auto;
+        }
+        .center{
+            display: flex;
+            justify-content: center;
+        }
+        .divider{
+            border-top:0.5px solid #ddd;
+            margin: 8px 0;
+        }
+        
+    </style>
+</head>
+<body>
+    <h1>Week2作業-履歷</h1>
+    <img src="tingting.png" alt="帥照">
+
+    <table>
+        <tr>
+            <th>項目</th>
+            <th>内容</th>
+        </tr>
+        <tr>
+            <td>姓名</td>
+            <td>丁丁</td>
+        </tr>
+        <tr>
+            <td>年齡</td>
+            <td>目測大概三到四瓶的15年johny walker</td>
+        </tr>
+        <tr>
+            <td>學歷</td>
+            <td>
+            英國約克大學電腦科學博士<hr class="divider">
+            彰化師範大學資訊管理學系碩士<hr class="divider">
+            中國文化大學企業管理學系學士
+        </td>
+        </tr>
+        <tr>
+            <td>現職</td>
+            <td>國立高雄大學資訊管理學系教授<hr class="divider">
+                國立高雄大學社會網路創新中心主任<hr class="divider">
+                馬來西亞泰萊大學客座教授<hr class="divider">
+                泰國詩納卡寧威洛大學客座教授<hr class="divider">
+                泰國蘭實大學客座教授
+            </td>
+        </tr>
+        <tr>
+            <td>技能</td>
+            <td>網頁程式設計<hr class="divider">
+                JAVA撰寫<hr class="divider">
+                計算機概論<hr class="divider">
+        
+                喝酒（不容質疑）
+            </td>
+        </tr>
+        <tr>
+            <td>更多關於丁丁~</td>
+            <td><a href="https://www.derrickting.pro/">
+                丁丁的部落格
+            </a><hr class="divider">
+            <a href = "https://www.facebook.com/ihsien.ting">丁丁的臉書
+            </a>
+            
+            
+    </table>
+</body>
+</html>
